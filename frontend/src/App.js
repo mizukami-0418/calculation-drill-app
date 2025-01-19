8330d261c4595223337c61fc147b7dc6aad74fac
mzkm2843 <ff10mm11yy23tamk@gmail.com>
2025-01-19 16:37:05 +0900

    初期設定ファイルと基本的なモデル、ビュー、テストを追加

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Calculation App</h1>
+      <button>スタート</button>
+    </div>
+  );
+}
+
+export default App;
